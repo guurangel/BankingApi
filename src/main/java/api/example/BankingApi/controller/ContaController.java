c39631ced9d2ca5c74507d45082b449d9e99d181
Gustavo <gustavo_dgk@outlook.com>
2025-03-13 23:32:38 -0300

    Adicionando Buscas por Id e CPF

--- src/main/java/api/example/BankingApi/controller/ContaController.java
+++ src/main/java/api/example/BankingApi/controller/ContaController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
@@ -16,7 +17,8 @@ public class ContaController {
     // Endpoint inicial "/"
     @GetMapping
     public String home() {
-        return "Projeto: Banking API - Integrantes: Gustavo Rangel, Luis Felippe Morais das Neves";
+        return "Projeto: Banking API - Integrantes: Gustavo Rangel, Luis Felippe Morais das Neves\r\n" + //
+                        "";
     }
 
     // Criar conta (POST /contas)
@@ -28,9 +30,31 @@ public class ContaController {
         return ResponseEntity.ok(conta);
     }
 
-    // Listar contas (GET /contas)
+    // Listar todas as contas (GET /contas)
     @GetMapping("/contas")
     public ResponseEntity<List<Conta>> listarContas() {
         return ResponseEntity.ok(contas);
+    }
+
+    // Buscar conta por ID (GET /contas/{id})
+    @GetMapping("/contas/{id}")
+    public ResponseEntity<Conta> buscarContaPorId(@PathVariable Long id) {
+        Optional<Conta> contaEncontrada = contas.stream()
+                .filter(conta -> conta.getId().equals(id))
+                .findFirst();
+
+        return contaEncontrada.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Buscar conta por CPF (GET /contas/cpf/{cpf})
+    @GetMapping("/contas/cpf/{cpf}")
+    public ResponseEntity<Conta> buscarContaPorCpf(@PathVariable String cpf) {
+        Optional<Conta> contaEncontrada = contas.stream()
+                .filter(conta -> conta.getCpf().equals(cpf))
+                .findFirst();
+
+        return contaEncontrada.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
