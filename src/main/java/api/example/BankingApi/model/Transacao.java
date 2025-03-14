f01933abfd6aae7a98a2c3f3b1c3a150c30af896
Gustavo <gustavo_dgk@outlook.com>
2025-03-14 10:12:50 -0300

    Adicionando Transacao model

---
+++ src/main/java/api/example/BankingApi/model/Transacao.java
@@ -0,0 +1,14 @@
+package api.example.BankingApi.model;
+
+public class Transacao {
+
+    private double valor;
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
