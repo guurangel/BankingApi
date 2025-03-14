6007158e989f6cc00aa9a843e4d4f4a4dd203e82
Gustavo <gustavo_dgk@outlook.com>
2025-03-14 08:38:13 -0300

    Adicionando mensagem de erro no enum

--- src/main/java/api/example/BankingApi/model/Tipos.java
+++ src/main/java/api/example/BankingApi/model/Tipos.java
@@ -1,5 +1,23 @@
 package api.example.BankingApi.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 public enum Tipos {
-    Corrente, Poupança, Salário
+    CORRENTE, POUPANCA, SALARIO;
+
+    @JsonCreator
+    public static Tipos fromString(String valor) {
+        // Verifica se o valor enviado no corpo do post não é nulo, se for nulo cai na exception.
+        if (valor == null) {
+            throw new IllegalArgumentException("O tipo de conta não pode ser nulo.");
+        }
+        // Converte as entradas para maiúsculo e verifica se o contéudo é igual ao definido no escopo do enum Tipos. Caso não seja idêntico ao definido no enum, retornado uma exception.
+        try {
+            return Tipos.valueOf(valor.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(
+                "Tipo de conta inválido! Use um dos seguintes valores: CORRENTE, POUPANCA ou SALARIO."
+            );
+        }
+    }
 }
